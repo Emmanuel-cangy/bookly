29bd012fa03c0a4bc388b90af03dc3ad89f5e6a9
Emmanuel Cangy <ecangy0102@gmail.com>
2022-05-30 17:17:05 +0400

    error corrections

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_30_122115) do
+ActiveRecord::Schema.define(version: 2022_05_30_122116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "rent_date"
+    t.date "return_date"
+    t.bigint "book_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_bookings_on_book_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.bigint "book_id", null: false
+    t.bigint "wishlist_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_bookmarks_on_book_id"
+    t.index ["wishlist_id"], name: "index_bookmarks_on_wishlist_id"
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "price_per_day"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +57,18 @@ ActiveRecord::Schema.define(version: 2022_05_30_122115) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "wishlists", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_wishlists_on_user_id"
+  end
+
+  add_foreign_key "bookings", "books"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "bookmarks", "books"
+  add_foreign_key "bookmarks", "wishlists"
+  add_foreign_key "books", "users"
+  add_foreign_key "wishlists", "users"
 end
