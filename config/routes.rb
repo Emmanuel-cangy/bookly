f962561d8d79291ca33e78a09772e9cbc46e1cee
Adel Narod <adelnarod@gmail.com>
2022-05-30 17:49:20 +0400

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'books#index'
+
+  resources :books do
+    resources :bookings, only: %i[new create]
+  end
+
+  resources :bookings, only: %i[destroy index]
+
+  resources :wishlists, only: %i[index show new create destroy] do
+    resources :bookmarks, only: %i[new create destroy]
+  end
 end
