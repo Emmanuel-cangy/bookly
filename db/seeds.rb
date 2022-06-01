9b2caa7ce22f71f3a07dc953e74152e7d2e9bd75
Emmanuel Cangy <ecangy0102@gmail.com>
2022-06-01 16:51:48 +0400

    corected seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,14 +45,3 @@ book3 = Book.create(
   But what if this neighbor cannot be relied upon. Or what if the situation is worse than it appears. How could an ordinary boy be this important to the situation. No turning back now though.")
 file = URI.open('https://res.cloudinary.com/deqc25xt2/image/upload/v1654086730/image_3_dtvuxe.jpg')
 book3.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
-
-book4 = Book.create(
-  title: 'My book cover',
-  price_per_day: Faker::Commerce.price,
-  user_id: User.first.id,
-  address: "Mauritius",
-  description: "The unexciting life of a boy changes in an instant as a neighbor enters his life.
-  The neighbor claims the boy is actually stuck within a dream world, a world from which there is only one way out and this 'dream person' will show the way. Unsure about everything, including this neighbor, the boy hesitantly agrees to the proposal, it all feels very exciting, as if a big adventure was about to begin.
-  But what if this neighbor cannot be relied upon. Or what if the situation is worse than it appears. How could an ordinary boy be this important to the situation. No turning back now though.")
-file = URI.open('https://res.cloudinary.com/deqc25xt2/image/upload/v1654086213/development/4uuoo1hbwxxqx8d9mb5xdj0wryku.jpg')
-book4.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
