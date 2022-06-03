1ac06c234a7106c6e441bf4e4b3ade4d18751b60
Adel Narod <adelnarod@gmail.com>
2022-06-03 11:47:00 +0400

    A little AJAX and new bookmark form in wishlist show

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,21 +1,27 @@
 class BookmarksController < ApplicationController
-  before_action :set_wishlist, only: %i[new create destroy]
+  before_action :set_wishlist, only: %i[new create]
+  helper_method :find_bookmark
+
 
   def new
     @bookmark = Bookmark.new
+    @books = @wishlist.books
   end
 
   def create
+    @books = @wishlist.books
     @bookmark = Bookmark.new(params_bookmark)
     @bookmark.wishlist = @wishlist
     if @bookmark.save
       redirect_to wishlist_path(@wishlist)
     else
-      render :new
+      # render :new
+      render 'wishlists/show'
     end
   end
 
   def destroy
+    @wishlist = Wishlist.find(params[:wishlist_id])
     @bookmark = Bookmark.find(params[:id])
     @bookmark.delete
     redirect_to wishlist_path(@wishlist)
@@ -29,5 +35,12 @@ class BookmarksController < ApplicationController
 
   def set_wishlist
     @wishlist = Wishlist.find(params[:wishlist_id])
+  end
+
+  def find_bookmark(book)
+    book.bookmarks.each do |bookmark|
+      @bookmark = bookmark if bookmark.wishlist_id == @wishlist.id
+    end
+    @bookmark
   end
 end
--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -1,7 +1,7 @@
 class Wishlist < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
-  has_many :bookmarks, dependent: :delete_all
+  has_many :bookmarks, dependent: :destroy
   has_many :books, through: :bookmarks
   validates :name, presence: true
 end
